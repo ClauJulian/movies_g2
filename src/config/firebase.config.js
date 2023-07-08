51681eebd6221da7d8496f67088784c38189d2a3
Cristian <cristian.zolkowski@gmail.com>
2023-07-07 21:47:54 -0300

    login firebase

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,3 +1,4 @@
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
@@ -14,5 +15,5 @@ const firebaseConfig = {
 
 
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const Auth = getAuth(app);
 const analytics = getAnalytics(app);

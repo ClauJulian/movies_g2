e535dfbcbe3c2087ac38209a41a729122b9b6fb5
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2023-07-13 16:02:08 -0300

    adaptaciones_login

--- src/pages/login/components/SingInForm.jsx
+++ src/pages/login/components/SingInForm.jsx
@@ -1,47 +1,9 @@
-import React from "react";
-import { useAuth } from "../../../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
-import { signUp } from "../services/signUp.service";
-import { signIn } from "../services/signIn.service";
+import React from 'react'
 
-const SignInForm = () => {
-  const navigate = useNavigate();
-
-  const { login } = useAuth();
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-
-    const form = new FormData(event.target);
-
-    const data = Object.fromEntries(form.entries());
-
-    const result = await signIn(data.email.toString(), data.password.toString());
-
-    login({
-      email: data.email.toString(),
-    });
-
-    navigate("/", {
-      replace: true,
-    });
-  };
-
+const SingInForm = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <input type="password" name="password" />
-      </div>
+    <div>SingInForm</div>
+  )
+}
 
-      <div>
-        <button type="submit">Iniciar Sesión</button>
-      </div>
-    </form>
-  );
-};
-
-export default SignInForm;
+export default SingInForm
--- src/pages/login/views/LoginView.jsx
+++ src/pages/login/views/LoginView.jsx
@@ -2,11 +2,14 @@ import  { useState, useEffect } from 'react';
 import useSWR from 'swr';
 import BannerLogin from '../../../components/bannerLogin/BannerLogin';
 import { getPopularMovies} from '../../../services/tmdb.services';
-
+//Clau
+import { Card, Spacer, Text } from "@nextui-org/react";
+import SignUpForm from '../components/SignUpForm';
+import SingInForm from '../components/SingInForm';
 
 
 const LoginView = () => {
-
+//Euge
   const { data: popularMovies, isLoading: popularMoviesIsLoading } = useSWR(
     "getPopularMovies",
     () => getPopularMovies()
@@ -36,6 +39,44 @@ const LoginView = () => {
     ]);
 
 
+//Clau
+  const [isRegister, setIsRegister] = useState(false);
+
+  const s = {
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    },
+    abs: {
+      position: "absolute",
+    },
+    card: {
+      display: "flex",
+      position: "fixed",
+      left: "50%",
+      top: "50%",
+      transform: "translate(-50%, -50%)",
+      flexDirection: "column",
+      width: "30vw",
+      minWidth: "500px",
+      maxWidth: "600px",
+      backdropFilter: "blur(20px)",
+      backgroundColor: "rgba(0,0,0,0.3)",
+      border: "1px solid rgba(255,255,255,0.1)",
+    },
+    title: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    hiperlink: {
+      cursor: "pointer",
+    },
+  };
+
+
   return (
 <>
 
@@ -56,8 +97,144 @@ const LoginView = () => {
     </div>
   
 
+
+
+  {/* // clau */}
+
+       <div  
+      style={{
+      marginTop: "100px",
+      width: "100vw",
+    }}>
+        
+         
+         <Card css={s.card}>
+               <Card.Body>
+                 <div style={s.title}>
+                  
+                   <Text h2>
+                     {isRegister
+                       ? "Registrate en Movie App"
+                      : "Bienvenido a Movie App"}
+                   </Text>
+                   <Text
+                    h3
+                    color="primary"
+                    onClick={() => setIsRegister(!isRegister)}
+                    css={s.hiperlink}
+                  >
+                     {isRegister
+                       ? "o puedes Inicia sesión aquí"
+                       : "o puedes Regístrarte aquí"}
+                   </Text>
+                 </div>
+
+                 {isRegister ? <SignUpForm /> : <SingInForm />}
+               </Card.Body>
+             </Card> 
+            
+           
+
+  
+      
+
+//     </div>
+
+
+ 
+
   </>
 );
 };
 
 export default LoginView;
+
+
+// import React, {useState} from 'react';
+// import { Card, Spacer, Text } from "@nextui-org/react";
+// import SignUpForm from '../components/SignUpForm/SignUpForm';
+// import SignInForm from '../components/SignInForm/SignInForm';
+
+
+// const LoginView = () => {
+
+//   const [isRegister, setIsRegister] = useState(false);
+
+//   const s = {
+//     container: {
+//       display: "flex",
+//       justifyContent: "center",
+//       alignItems: "center",
+//       height: "100vh",
+//     },
+//     abs: {
+//       position: "absolute",
+//     },
+//     card: {
+//       display: "flex",
+//       position: "fixed",
+//       left: "50%",
+//       top: "50%",
+//       transform: "translate(-50%, -50%)",
+//       flexDirection: "column",
+//       width: "30vw",
+//       minWidth: "500px",
+//       maxWidth: "600px",
+//       backdropFilter: "blur(20px)",
+//       backgroundColor: "rgba(0,0,0,0.3)",
+//       border: "1px solid rgba(255,255,255,0.1)",
+//     },
+//     title: {
+//       display: "flex",
+//       flexDirection: "column",
+//       alignItems: "center",
+//     },
+//     hiperlink: {
+//       cursor: "pointer",
+//     },
+//   };
+
+//   return (
+//     <div  
+//       style={{
+//       marginTop: "100px",
+//       width: "100vw",
+//     }}>
+        
+         
+//         <Card css={s.card}>
+//               <Card.Body>
+//                 <div style={s.title}>
+                  
+//                   <Text h2>
+//                     {isRegister
+//                       ? "Registrate en Movie App"
+//                       : "Bienvenido a Movie App"}
+//                   </Text>
+//                   <Text
+//                     h3
+//                     color="primary"
+//                     onClick={() => setIsRegister(!isRegister)}
+//                     css={s.hiperlink}
+//                   >
+//                     {isRegister
+//                       ? "o puedes Inicia sesión aquí"
+//                       : "o puedes Regístrarte aquí"}
+//                   </Text>
+//                 </div>
+
+//                 {isRegister ? <SignUpForm /> : <SignInForm />}
+//               </Card.Body>
+//             </Card>
+            
+           
+
+  
+      
+
+//     </div>
+
+//   )
+// }
+
+// export default LoginView

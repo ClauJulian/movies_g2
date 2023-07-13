c06d15f4472ff8e3dfc6315721e8ed66baa9e955
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2023-07-13 16:05:56 -0300

    login

--- src/pages/login/components/SignUpForm.jsx
+++ src/pages/login/components/SignUpForm.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React from 'react';
+import CustomInput from "../../../components/CustomInput/CustomInput";
+import { Button, Image, Spacer } from "@nextui-org/react";
+// import useLogin from '../../hooks/useLogin';
 
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
-};
 
-export default SignUpForm;
+    // const { form, setForm, signUpEmail } = useLogin();
+
+  return (
+    <div>      
+        <form
+        // onSubmit={signUpEmail}
+        style={{
+        display: "flex",
+        flexDirection: "column",
+        }}
+        >
+          <CustomInput
+            label={"Correo electrónico"}
+            type={"email"}
+            name={"email"}
+            placeholder={"movie_app @ movieapp.com"}
+            // onChange={(e) => setForm({ ...form, email: e.target.value })}
+          />
+          <Spacer y={2} />
+          <CustomInput
+            label={"Contraseña"}
+            type={"password"}
+            name={"password"}
+            placeholder={"* * * * * * * *"}
+            // onChange={(e) => setForm({ ...form, password: e.target.value })}
+          />
+          <Spacer y={2} />
+          <Button type="submit" color={"secondary"}>
+            Registrarse
+          </Button>
+          <Spacer y={1} />
+
+  </form>
+  
+  </div>
+  )
+}
+
+export default SignUpForm

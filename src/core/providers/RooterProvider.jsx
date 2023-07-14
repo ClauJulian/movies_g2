0cb74f0ec4c24ef7b891f541d8be1c3f0c0bcf6f
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2023-07-14 04:56:46 -0300

    Clear

--- src/core/providers/RooterProvider.jsx
+++ src/core/providers/RooterProvider.jsx
@@ -2,20 +2,17 @@ import { NextUIProvider } from '@nextui-org/react'
 import React from 'react'
 import FavoritesProvider from '../../providers/FavoritesProvider'
 import AuthProvider from '../auth/providers/AuthProvider'
-// import UserProvider from '../auth/providers/UserProvider'
 import { dark_Theme } from '../themes/themes_nextui'
 
 
 export const RooterProvider = ({children}) => {
   return (
     <NextUIProvider theme={dark_Theme}>
-      {/* <UserProvider> */}
         <AuthProvider>     
         <FavoritesProvider>
           <div>{children}</div>
         </FavoritesProvider>
         </AuthProvider>
-      {/* </UserProvider> */}
     </NextUIProvider>
   )
 }

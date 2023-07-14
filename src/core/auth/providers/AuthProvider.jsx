288b870556849a61102ed6fe7efd62ee31dade50
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2023-07-14 03:05:29 -0300

    Login

---
+++ src/core/auth/providers/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import React, { useReducer } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { authInit, authInitialState, authReducer } from "../reducers/authReducers";
+
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, authInitialState,authInit);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
